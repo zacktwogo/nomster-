a07dba9b6ec72423fc5454d1b1ad3529c53a9183
Zack V <zacktwogo@gmail.com>
2019-10-05 00:08:16 +0000

    Added Bootsrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= rewuire tether
+//= require bootstrap-sprockets
 //= require_tree .
