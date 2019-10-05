0662aec40eecb42762796bed8dbb4073456bc911
Zack V <zacktwogo@gmail.com>
2019-10-05 23:13:55 +0000

    Creating the place form

--- Gemfile
+++ Gemfile
@@ -69,3 +69,7 @@ gem 'bootstrap', '4.0.0.alpha6'
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
+
+gem 'bootstrap-will_paginate', '~> 1.0'
+gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.2'
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,11 @@
 class PlacesController < ApplicationController
 
     def index
-        @places = Place.all
+        @place = Place.all
+        .paginate(page: params[:page], per_page: 10)
+    end
+
+    def new
+        @place = Place.new
     end
 end
