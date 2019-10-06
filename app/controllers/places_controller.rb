7accbc41c492524c8b731fd509ae24ed9b189561
Zack V <zacktwogo@gmail.com>
2019-10-06 01:05:47 +0000

    Handle form submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,21 @@
 class PlacesController < ApplicationController
-
     def index
-        @place = Place.all
-        .paginate(page: params[:page], per_page: 10)
+      @places = Place.all
+      .paginate(page: params[:page], per_page: 10)
     end
-
+  
     def new
-        @place = Place.new
+      @place = Place.new
     end
-end
+  
+    def create
+      Place.create(place_params)
+      redirect_to root_path
+    end
+  
+    private
+  
+    def place_params
+      params.require(:place).permit(:name, :description, :address)
+    end
+  end
